60bb173adedfd4ca64d2cea490acf6ef36c91edd
CJ <silvermc@lafayette.edu>
2018-11-30 14:59:19 -0500

    Added truck and things to shipment

--- Shipment.java
+++ Shipment.java
@@ -9,5 +9,16 @@ public class Shipment
 {
     String destination;
     ArrayList<Cargo> shipmentCargo = new ArrayList<Cargo>();
-    
+    int weight;
+    public Shipment(String d){
+        destination = d;
+        weight = 0;
+    }
+    public boolean addCargo(Cargo c){
+        if(weight + c.weight > 500){
+            return false;
+        }
+        shipmentCargo.add(c);
+        return true;
+    }
 }
