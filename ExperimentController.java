21bd44fd0610e8918c77abc46b9f4a7a67236359
CJ <silvermc@lafayette.edu>
2018-12-05 09:46:29 -0500

    Order of shipments working!

--- ExperimentController.java
+++ ExperimentController.java
@@ -34,6 +34,7 @@ public class ExperimentController
         ArrayList<Truck> dispatch = new ArrayList<Truck>();
         int truckNumber=1;
         int incompleteWarehouses = ec.cargoList.size();
+        int startingWarehouse = 0;
         while(incompleteWarehouses != 0){
             Truck t = new Truck(truckNumber);
             truckNumber++;
@@ -41,31 +42,30 @@ public class ExperimentController
                 int r = 5;
             }
             Shipment s = new Shipment(ec.cargoList.get(0).city,500);
-            int warehouseNumber =0;
+            Warehouse currentWarehouse = ec.cargoList.get(startingWarehouse);
             while(!t.truckReady){
                 boolean shipmentCheck = false;
                 s.setDestination(ec.cityMap.getVertex(s.location));
                 
                 s.destination.setClosestCities(ec.cityMap.shortestPath(s.destination));
                 int limit = ec.cargoList.size();
-                while(!shipmentCheck && warehouseNumber <limit){
+                while(!shipmentCheck){
                     //This needs to be changed not only to access the first warehouse
                     //In the list
-                    if(ec.cargoList.get(warehouseNumber).incomingCargo.isEmpty()){
-                        Warehouse w = ec.cargoList.get(warehouseNumber);
+                    if(currentWarehouse.incomingCargo.isEmpty()){
                         //warehouseNumber++;
-                        System.out.println(w);
-                        w.location.hasWarehouse = false;
+                        System.out.println(currentWarehouse);
+                        currentWarehouse.location.hasWarehouse = false;
                         shipmentCheck = true;
-                        if(!w.complete){
+                        if(!currentWarehouse.complete){
                             incompleteWarehouses--;
                         }
-                        w.complete = true;
+                        currentWarehouse.complete = true;
                         continue;
                     }
-                    Cargo c = ec.cargoList.get(warehouseNumber).incomingCargo.remove(0);
+                    Cargo c = currentWarehouse.incomingCargo.remove(0);
                     if(!s.addCargo(c)){
-                        ec.cargoList.get(warehouseNumber).incomingCargo.add(0,c);
+                        currentWarehouse.incomingCargo.add(0,c);
                         shipmentCheck = true;
                     }
                 }
@@ -75,12 +75,12 @@ public class ExperimentController
                 shipmentCheck = false;
                 t.truckReady = true;
                 ArrayList<City> closest = s.destination.closestCities;
-                for(int i = warehouseNumber+1;i<closest.size();i++){
+                for(int i = 0;i<closest.size();i++){
                     if(closest.get(i).hasWarehouse){
                         if(!closest.get(i).cityWarehouse.incomingCargo.isEmpty() && closest.get(i).cityWarehouse.incomingCargo.get(0).weight <= 500-t.weight){
                             t.truckReady = false;
                             s = new Shipment(closest.get(i).name,500-t.weight);
-                            warehouseNumber = i;
+                            currentWarehouse = closest.get(i).cityWarehouse;
                             break;
                         }
                         else{
@@ -98,9 +98,12 @@ public class ExperimentController
             dispatch.add(t);
             System.out.println("Truck sent");
         }
+        int totalDist = 0;
         for(Truck t : dispatch){
             System.out.println(t);
+            totalDist+=t.distanceTraveled;
         }
+        System.out.println("Total Distance: " + totalDist);
     }
     public ArrayList<Warehouse> mapCity(){
         try{
